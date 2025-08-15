900d2fe4beb5fe83259a2a0e449c48134a48bc18
Lutfi Alvaro Pratama <78428836+lutfialvarop@users.noreply.github.com>
2025-08-15 10:00:36 +0700

    feat: add jwt and register plugins

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const Hapi = require("@hapi/hapi");
+const Jwt = require("@hapi/jwt");
 
 const albums = require("./api/albums");
 const AlbumsService = require("./services/postgres/AlbumsService");
@@ -8,11 +9,33 @@ const AlbumsValidator = require("./validator/albums");
 const songs = require("./api/songs");
 const SongsService = require("./services/postgres/SongsService");
 const SongsValidator = require("./validator/songs");
+
+const users = require("./api/users");
+const UsersService = require("./services/postgres/UsersService");
+const UsersValidator = require("./validator/users");
+
+const authentications = require("./api/authentications");
+const AuthenticationsService = require("./services/postgres/AuthenticationsService");
+const AuthenticationsValidator = require("./validator/authentications");
+const TokenManger = require("./tokenize/TokenManager");
+
+const playlists = require("./api/playlists");
+const PlaylistsService = require("./services/postgres/PlaylistsService");
+const PlaylistsValidator = require("./validator/playlists");
+
+const collaborations = require("./api/collaborations");
+const CollaborationsService = require("./services/postgres/CollaborationsService");
+const CollaborationsValidator = require("./validator/collaborations");
+
 const ClientError = require("./exceptions/ClientError");
 
 const init = async () => {
     const albumsService = new AlbumsService();
     const songsService = new SongsService();
+    const usersService = new UsersService();
+    const authenticationsService = new AuthenticationsService();
+    const collaborationsService = new CollaborationsService();
+    const playlistsService = new PlaylistsService(songsService, collaborationsService);
 
     const server = Hapi.server({
         port: process.env.PORT || 3000,
@@ -22,6 +45,28 @@ const init = async () => {
                 origin: ["*"],
             },
         },
+    });
+
+    await server.register([
+        {
+            plugin: Jwt,
+        },
+    ]);
+
+    server.auth.strategy("openmusic_jwt", "jwt", {
+        keys: process.env.ACCESS_TOKEN_KEY,
+        verify: {
+            aud: false,
+            iss: false,
+            sub: false,
+            maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+        },
+        validate: (artifacts) => ({
+            isValid: true,
+            credentials: {
+                id: artifacts.decoded.payload.id,
+            },
+        }),
     });
 
     await server.register([
@@ -39,31 +84,50 @@ const init = async () => {
                 validator: SongsValidator,
             },
         },
+        {
+            plugin: users,
+            options: {
+                service: usersService,
+                validator: UsersValidator,
+            },
+        },
+        {
+            plugin: authentications,
+            options: {
+                authenticationsService,
+                usersService,
+                tokenManager: TokenManger,
+                validator: AuthenticationsValidator,
+            },
+        },
+        {
+            plugin: playlists,
+            options: {
+                service: playlistsService,
+                validator: PlaylistsValidator,
+            },
+        },
+        {
+            plugin: collaborations,
+            options: {
+                service: collaborationsService,
+                playlistsService,
+                usersService,
+                validator: CollaborationsValidator,
+            },
+        },
     ]);
 
     server.ext("onPreResponse", (request, h) => {
         const { response } = request;
 
-        if (response instanceof Error) {
-            if (response instanceof ClientError) {
-                return h
-                    .response({
-                        status: "fail",
-                        message: response.message,
-                    })
-                    .code(response.statusCode);
-            }
-
-            if (!response.isServer) {
-                return h.continue;
-            }
-
+        if (response instanceof ClientError) {
             return h
                 .response({
-                    status: "error",
-                    message: "Internal Server Error",
+                    status: "fail",
+                    message: response.message,
                 })
-                .code(500);
+                .code(response.statusCode);
         }
 
         return h.continue;
