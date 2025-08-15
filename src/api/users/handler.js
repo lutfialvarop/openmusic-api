4231d52fc78df41c5072618a4379a440abedf7ed
Lutfi Alvaro Pratama <78428836+lutfialvarop@users.noreply.github.com>
2025-08-15 10:02:00 +0700

    feat: add route and handler user

---
+++ src/api/users/handler.js
@@ -0,0 +1,27 @@
+class UsersHandler {
+    constructor(service, validator) {
+        this._service = service;
+        this._validator = validator;
+
+        this.addUser = this.addUser.bind(this);
+    }
+
+    async addUser(request, h) {
+        this._validator.validateUserPayload(request.payload);
+        const { username, password, fullname } = request.payload;
+
+        const userId = await this._service.addUser({ username, password, fullname });
+
+        return h
+            .response({
+                status: "success",
+                message: "User successfully added",
+                data: {
+                    userId,
+                },
+            })
+            .code(201);
+    }
+}
+
+module.exports = UsersHandler;
---
+++ src/api/users/routes.js
@@ -0,0 +1,9 @@
+const routes = (handler) => [
+    {
+        method: "POST",
+        path: "/users",
+        handler: handler.addUser,
+    },
+];
+
+module.exports = routes;
